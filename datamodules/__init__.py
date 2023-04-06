8d062a2d3a68deef00cf99201ae14a3e13aafa7c
Ming Shan Hee <mingshan.hee@gmail.com>
2023-04-06 06:23:39 +0000

    Add T5 Model for Generative Approach

--- datamodules/__init__.py
+++ datamodules/__init__.py
@@ -1,7 +1,7 @@
-from .fhm import FHMDataModule
+from .fhm import FHMDataModule, FHMT5DataModule
 
 def load_datamodule(dataset_name, model_class_or_path, **kwargs):
     if "fhm" in dataset_name:
-        return FHMDataModule(dataset_name, model_class_or_path, **kwargs)
+        return FHMT5DataModule(dataset_name, model_class_or_path, **kwargs)
     else:
         raise NotImplementedError(f"'{dataset_name}' datamodule not implemented")
--- datamodules/utils.py
+++ datamodules/utils.py
@@ -17,3 +17,27 @@ def image_collate_fn(batch, processor):
         inputs['labels'] = torch.tensor(labels, dtype=torch.int64)
 
     return inputs
+
+def text_collate_fn(batch, tokenizer):
+    texts = []
+    for item in batch:
+        texts.append(item["text"])
+    
+    inputs = tokenizer(  
+        text=texts, return_tensors="pt", padding=True
+    )
+
+    # Get Labels
+    label_name = "label"
+    if label_name in batch[0].keys():
+        labels = [feature[label_name] for feature in batch]
+
+        if isinstance(labels[0], str):
+            labels = tokenizer(labels, truncation=True, return_tensors="pt").input_ids
+            labels[labels == tokenizer.pad_token_id] = -100
+        else:
+            labels = torch.tensor(labels, dtype=torch.int64)
+        
+        inputs['labels'] = labels
+
+    return inputs
