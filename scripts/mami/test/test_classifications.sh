1365ad7ae7ac06e6278586cb0358a210db6472d8
SavageSanta11 <codetechie11@gmail.com>
2023-06-23 05:03:45 +0700

    added configs with commonly accessible annotation filepaths

--- scripts/mami/test/test_classifications.sh
+++ scripts/mami/test/test_classifications.sh
@@ -1,27 +1,58 @@
-echo "FLAVA - MAMI"
+echo "FLAVA - MAMI - TASK A"
 
 python3 main.py fit \
-    --config configs/mami/flava.yaml \
+    --config configs/mami/subtask_a/flava.yaml \
     --seed_everything 1111 \
     --trainer.devices 1 \
     --trainer.max_epochs 2 \
     --trainer.limit_train_batches 5 \
     --trainer.limit_val_batches 2
 
-echo "LXMERT - MAMI"
+echo "LXMERT - MAMI - TASK A"
 
 python3 main.py fit \
-    --config configs/mami/lxmert_features.yaml \
+    --config configs/mami/subtask_a/lxmert_features.yaml \
     --seed_everything 1111 \
     --trainer.devices 1 \
     --trainer.max_epochs 2 \
     --trainer.limit_train_batches 5 \
     --trainer.limit_val_batches 2
 
-echo "VisualBERT - MAMI"
+echo "VisualBERT - MAMI - TASK A"
 
 python3 main.py fit \
-    --config configs/mami/visualbert_features.yaml \
+    --config configs/mami/subtask_a/visualbert_features.yaml \
+    --seed_everything 1111 \
+    --trainer.devices 1 \
+    --trainer.max_epochs 2 \
+    --trainer.limit_train_batches 5 \
+    --trainer.limit_val_batches 2
+
+
+echo "FLAVA - MAMI - TASK B"
+
+python3 main.py fit \
+    --config configs/mami/subtask_b/flava.yaml \
+    --seed_everything 1111 \
+    --trainer.devices 1 \
+    --trainer.max_epochs 2 \
+    --trainer.limit_train_batches 5 \
+    --trainer.limit_val_batches 2
+
+echo "LXMERT - MAMI - TASK B"
+
+python3 main.py fit \
+    --config configs/mami/subtask_b/lxmert_features.yaml \
+    --seed_everything 1111 \
+    --trainer.devices 1 \
+    --trainer.max_epochs 2 \
+    --trainer.limit_train_batches 5 \
+    --trainer.limit_val_batches 2
+
+echo "VisualBERT - MAMI - TASK B"
+
+python3 main.py fit \
+    --config configs/mami/subtask_b/visualbert_features.yaml \
     --seed_everything 1111 \
     --trainer.devices 1 \
     --trainer.max_epochs 2 \
---
+++ scripts/mami/train/subtask_b/flava.sh
@@ -0,0 +1,1 @@
+python3 main.py fit --config configs/mami/subtask_b/flava.yaml --seed_everything 1111
