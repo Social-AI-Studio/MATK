a0b62d1097d9c7a551eec59a86cb07e7af8db41f
mingshanhee <mingshan.hee@gmail.com>
2023-09-06 09:12:24 -0400

    Updated Lxmert model across the four datasets

--- models/lxmert.py
+++ models/lxmert.py
@@ -1,67 +1,51 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import lightning.pytorch as pl
-from lightning.pytorch.callbacks import Callback
-import torchmetrics
 import importlib
 from transformers import LxmertModel
 
 from datamodules.collators.gqa_lxmert.modeling_frcnn import GeneralizedRCNN
 from datamodules.collators.gqa_lxmert.lxmert_utils import Config
 
+from .model_utils import setup_metrics
+from .base import BaseLightningModule
 
-class LxmertClassificationModel(pl.LightningModule):
-    def __init__(self, 
-                 model_class_or_path, 
-                 cls_dict,
-                 metrics: list,
-                 frcnn_class_or_path=None,
-                 ):
+class LxmertClassificationModel(BaseLightningModule):
+    def __init__(
+            self, 
+            model_class_or_path: str,
+            frcnn_class_or_path: str,
+            frcnn_trainable: bool,
+            metrics_cfg: dict,
+            cls_dict: dict,
+            optimizers: list
+        ):
         super().__init__()
         self.save_hyperparameters()
 
         self.model = LxmertModel.from_pretrained(model_class_or_path)
+        self.metric_names = [cfg.name.lower() for cfg in metrics_cfg.values()]
+        self.cls_dict = cls_dict
+        self.optimizers = optimizers
+
         if frcnn_class_or_path:
             self.frcnn_cfg = Config.from_pretrained("unc-nlp/frcnn-vg-finetuned")
             self.frcnn = GeneralizedRCNN.from_pretrained("unc-nlp/frcnn-vg-finetuned", config=self.frcnn_cfg)
 
-        # set up classification
+            if not frcnn_trainable:
+                for param in self.frcnn.parameters():
+                    param.requires_grad = False
+        
+        # set up metric
         self.mlps = nn.ModuleList([
-            nn.Linear(self.model.config.hidden_size, value) 
+            nn.Linear(self.model.config.hidden_size, value)
             for value in cls_dict.values()
         ])
         
         # set up metric
-        # set up metric
-        self.cls_dict = cls_dict
-        
-        # TEMP HACK
-        package_name = "torchmetrics"
-        module = importlib.import_module(package_name)
-        self.metric_dict = metrics # metric details : metric class
-        
-        for stage in ["train", "validate", "test"]:
-            for key, value in cls_dict.items():
-                for metric_details in self.metric_dict:
-                    metric_class = getattr(module, metric_details['name'])
-                    args = metric_details['args']
-                    args["num_classes"] = value
-                    setattr(self, f"{key}_{stage}_{metric_details['name']}", metric_class(**args))
-
-
-    def compute_metrics_and_logs(self, cls_name, stage, loss, targets, preds):
-        for metric_details in self.metric_dict:
-            metric_name = metric_details['name']
-            metric = getattr(self, f"{cls_name}_{stage}_{metric_name}")
-
-            if metric_name == 'Accuracy':
-                metric(preds.argmax(dim=-1), targets)
-            elif metric_name == 'AUROC':
-                metric(preds, targets)
-            self.log(f'{cls_name}_{stage}_{metric_name}', metric, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        self.log(f'{cls_name}_{stage}_loss', loss, prog_bar=True)
-
+        setup_metrics(self, cls_dict, metrics_cfg, "train")
+        setup_metrics(self, cls_dict, metrics_cfg, "validate")
+        setup_metrics(self, cls_dict, metrics_cfg, "test")
 
     def training_step(self, batch, batch_idx):
 
@@ -69,7 +53,11 @@ class LxmertClassificationModel(pl.LightningModule):
         token_type_ids = batch['token_type_ids']
         attention_mask = batch['attention_mask']
 
-        if hasattr(self, "frcnn"):
+        if "visual_feats" in batch and "visual_pos" in batch:
+            visual_feats = batch['visual_feats']
+            visual_pos = batch['visual_pos']
+        else:
+            # Run Faster-RCNN
             images = batch['images']
             sizes = batch['sizes']
             scales_yx = batch['scales_yx']
@@ -81,14 +69,12 @@ class LxmertClassificationModel(pl.LightningModule):
                 padding="max_detections",
                 max_detections=self.frcnn_cfg.max_detections,
                 return_tensors="pt",
+                location=images.device
             )
 
-            visual_feats = visual_dict['visual_feats']
-            visual_pos = visual_dict['visual_pos']
-        else:
-            visual_feats = batch['visual_feats']
-            visual_pos = batch['visual_pos']
-        
+            visual_feats = visual_dict['roi_features']
+            visual_pos = visual_dict.get("normalized_boxes")
+
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -97,25 +83,22 @@ class LxmertClassificationModel(pl.LightningModule):
             token_type_ids = token_type_ids
         )
 
+        total_loss = 0
 
-        loss = 0
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            targets = batch[cls_name]
+            preds = self.mlps[idx](outputs[0][:, 0])
 
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
+            loss = F.cross_entropy(preds, targets)
+            total_loss += loss
 
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            pooled_output = outputs[0][:, 0]  # Extract the [CLS] token embedding
-            label_preds = self.mlps[i](pooled_output)
-            label_loss = F.cross_entropy(label_preds, label_targets)
-            loss += label_loss
-            self.compute_metrics_and_logs(label_list[i], "train", label_loss, label_targets, label_preds)
+            self.compute_metrics_step(
+                cls_name, "train", loss, targets, preds)
+
+        return total_loss
         
-        return loss
-    
-    
     def validation_step(self, batch, batch_idx):
+
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
         token_type_ids = batch['token_type_ids']
@@ -136,11 +119,12 @@ class LxmertClassificationModel(pl.LightningModule):
                 padding="max_detections",
                 max_detections=self.frcnn_cfg.max_detections,
                 return_tensors="pt",
+                location=images.device
             )
 
-            visual_feats = visual_dict['visual_feats']
-            visual_pos = visual_dict['visual_pos']
-        
+            visual_feats = visual_dict['roi_features']
+            visual_pos = visual_dict.get("normalized_boxes")
+
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -149,30 +133,48 @@ class LxmertClassificationModel(pl.LightningModule):
             token_type_ids = token_type_ids
         )
 
+        total_loss = 0
 
-        loss = 0
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            targets = batch[cls_name]
+            preds = self.mlps[idx](outputs[0][:, 0])
 
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
+            loss = F.cross_entropy(preds, targets)
+            total_loss += loss
+            
+            self.compute_metrics_step(
+                cls_name, "validate", loss, targets, preds)
 
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            pooled_output = outputs[0][:, 0]  # Extract the [CLS] token embedding
-            label_preds = self.mlps[i](pooled_output)
-            label_loss = F.cross_entropy(label_preds, label_targets)
-            loss += label_loss
-            self.compute_metrics_and_logs(label_list[i], "validate", label_loss, label_targets, label_preds)
-        
-        return loss
+        return total_loss / len(self.cls_dict)
     
     def test_step(self, batch, batch_idx):
+
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
-        visual_feats = batch['visual_feats']
-        visual_pos = batch['visual_pos']
         token_type_ids = batch['token_type_ids']
-        
+
+        if "visual_feats" in batch and "visual_pos" in batch:
+            visual_feats = batch['visual_feats']
+            visual_pos = batch['visual_pos']
+        else:
+            # Run Faster-RCNN
+            images = batch['images']
+            sizes = batch['sizes']
+            scales_yx = batch['scales_yx']
+            
+            visual_dict = self.frcnn(
+                images,
+                sizes,
+                scales_yx=scales_yx,
+                padding="max_detections",
+                max_detections=self.frcnn_cfg.max_detections,
+                return_tensors="pt",
+                location=images.device
+            )
+
+            visual_feats = visual_dict['roi_features']
+            visual_pos = visual_dict.get("normalized_boxes")
+
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -181,30 +183,48 @@ class LxmertClassificationModel(pl.LightningModule):
             token_type_ids = token_type_ids
         )
 
+        total_loss = 0
 
-        loss = 0
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            targets = batch[cls_name]
+            preds = self.mlps[idx](outputs[0][:, 0])
 
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
+            loss = F.cross_entropy(preds, targets)
+            total_loss += loss
 
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            pooled_output = outputs[0][:, 0]  # Extract the [CLS] token embedding
-            label_preds = self.mlps[i](pooled_output)
-            label_loss = F.cross_entropy(label_preds, label_targets)
-            loss += label_loss
-            self.compute_metrics_and_logs(label_list[i], "test", label_loss, label_targets, label_preds)
+            self.compute_metrics_step(
+                cls_name, "test", loss, targets, preds)
         
         return loss
 
     def predict_step(self, batch, batch_idx):
+
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
-        visual_feats = batch['visual_feats']
-        visual_pos = batch['visual_pos']
         token_type_ids = batch['token_type_ids']
-        
+
+        if "visual_feats" in batch and "visual_pos" in batch:
+            visual_feats = batch['visual_feats']
+            visual_pos = batch['visual_pos']
+        else:
+            # Run Faster-RCNN
+            images = batch['images']
+            sizes = batch['sizes']
+            scales_yx = batch['scales_yx']
+            
+            visual_dict = self.frcnn(
+                images,
+                sizes,
+                scales_yx=scales_yx,
+                padding="max_detections",
+                max_detections=self.frcnn_cfg.max_detections,
+                return_tensors="pt",
+                location=images.device
+            )
+
+            visual_feats = visual_dict['roi_features']
+            visual_pos = visual_dict.get("normalized_boxes")
+
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -213,25 +233,27 @@ class LxmertClassificationModel(pl.LightningModule):
             token_type_ids = token_type_ids
         )
 
-
-        loss = 0
-
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
-
         results = {}
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            pooled_output = outputs[0][:, 0]  # Extract the [CLS] token embedding
-            label_preds = self.mlps[i](pooled_output)
-            results[label_list[i]] = label_preds
-        
-        if "labels" in batch:
-            results['labels'] = batch["labels"]
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            preds = self.mlps[idx](outputs[0][:, 0])
+            
+            results["img"] = batch["image_filename"].tolist()
+            results[f"{cls_name}_preds"] = torch.argmax(preds, dim=1).tolist()
+            results[f"{cls_name}_labels"] = batch[cls_name].tolist()
 
         return results
-    
+
     def configure_optimizers(self):
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=2e-5)
-        return [self.optimizer]
+        opts = []
+        for opt_cfg in self.optimizers:
+            class_name = opt_cfg.pop("class_path")
+            
+            package_name = ".".join(class_name.split(".")[:-1])
+            package = importlib.import_module(package_name)
+            
+            class_name = class_name.split(".")[-1]
+            cls = getattr(package, class_name)
+
+            opts.append(cls(self.parameters(), **opt_cfg))
+
+        return opts
